63b336ad02611cf5084bf9630f70b46c0e79f92e
marcoberdiano <68471261+marcoberdiano@users.noreply.github.com>
2022-05-09 12:27:47 +0100

    new component

---
+++ src/components/PropsDisplayer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+export class PropsDisplayer extends React.Component {
+  render() {
+  	const stringProps = JSON.stringify(this.props);
+
+    return (
+      <div>
+        <h1>CHECK OUT MY PROPS OBJECT</h1>
+        <h2>{stringProps}</h2>
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/index.css';
 import reportWebVitals from './components/reportWebVitals';
-import ProfilePage from './components/ProfilePage';
+import {PropsDisplayer} from './components/PropsDisplayer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   <ProfilePage />
+   <PropsDisplayer />
   </React.StrictMode>
 );
 
