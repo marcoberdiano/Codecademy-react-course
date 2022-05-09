b2e8b1a83fe5348f5d644209e28ce9b2b913ab6d
marcoberdiano <68471261+marcoberdiano@users.noreply.github.com>
2022-05-09 13:36:20 +0100

    add Talker.js and Button.js

---
+++ src/components/Talker.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Button } from './Button';
+
+
+
+class Talker extends React.Component {
+  talk () {
+    let speech = '';
+    for (let i = 0; i < 10000; i++) {
+      speech += 'blah ';
+    }
+    alert(speech);
+  }
+  render() {
+    return <Button />;
+  }
+}
+
+export default Talker;
