98228a9fd37e7297f3332b5758cb21c34c4afeea
marcoberdiano <68471261+marcoberdiano@users.noreply.github.com>
2022-05-09 13:22:21 +0100

    add new files Welcome.js and Home.js

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Welcome } from './Welcome';
+
+class Home extends React.Component {
+  render() {
+    return <Welcome name='Wolfgang Amadeus Mozart' />;
+  }
+}
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/index.css';
 import reportWebVitals from './components/reportWebVitals';
-import App from './components/App';
+import Home from './components/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   <App/>
+   <Home/>
   </React.StrictMode>
 );
 
