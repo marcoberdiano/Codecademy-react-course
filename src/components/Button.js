6f7f64d7036dbe51ee415ceb84faf890e011e2d0
marcoberdiano <68471261+marcoberdiano@users.noreply.github.com>
2022-05-09 13:57:29 +0100

    update Talker.js and Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import '../css/Button.css'
 
 export class Button extends React.Component {
   render() {
     return (
-      <button onClick={this.props.talk}>
-        Click me!
-      </button>
+      <div id="button-container">
+          <button id="button" onClick={this.props.talk}>
+            Click me!
+        </button>
+      </div>
     );
   }
 }
