56cf5d6b9942ef13a4f207e62376aec526fcfd1c
marcoberdiano <68471261+marcoberdiano@users.noreply.github.com>
2022-05-09 12:59:26 +0100

    change App.js and index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/index.css';
 import reportWebVitals from './components/reportWebVitals';
-import Greeting from './components/Greeting';
+import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   <Greeting name="Marco"/>
+   <App/>
   </React.StrictMode>
 );
 
